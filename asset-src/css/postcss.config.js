274f31555bc283740ffd6e04d28134e92b3bc6d3
Simon Clayson <simon@simonclayson.co.uk>
2021-02-15 07:05:09 +0000

    name asset-src

--- asset-src/css/postcss.config.js
+++ asset-src/css/postcss.config.js
@@ -3,7 +3,7 @@ const themeDir = __dirname + "/../../"
 module.exports = {
   plugins: [
     require("postcss-import")({path: [themeDir]}),
-    require("tailwindcss")(themeDir + "assets/css/tailwind.config.js"),
+    require("tailwindcss")(themeDir + "asset-src/css/tailwind.config.js"),
     require("autoprefixer")(),
   ],
 }
